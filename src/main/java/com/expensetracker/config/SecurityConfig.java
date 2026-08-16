bd75fbbb06a85109e0bf35999f42e1ed7b312632
prathameshLodekar <prathameshlodekar.com>
2026-08-16 14:20:48 +0530

    Updated Security Config

--- src/main/java/com/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/expensetracker/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/health-check").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
